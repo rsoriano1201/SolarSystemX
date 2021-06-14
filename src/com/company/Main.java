7ffb1d8bd2a1ba928b03f47d82d14391cbf61a1b
rsoriano1201 <rsoriano1201@gmail.com>
2021-06-14 06:01:29 -0700

    Solar system basic structure is ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
     public static void main(String[] args){
 
-
+SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun=1;
+        FactorX.Moon=3;
+        FactorX.Planets=2;
 
 
     }
